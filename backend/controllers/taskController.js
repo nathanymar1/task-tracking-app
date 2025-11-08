87f5a5c0423abb4e6f3a66939fceba6b2c56124c
Nathan Mar <nathanymar@gmail.com>
2025-11-07 17:00:56 -0800

    set up routes for backend

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+export const getTask = async (req, res) => {};
+export const getTasks = async (req, res) => {};
+export const createTask = async (req, res) => {};
+export const updateTask = async (req, res) => {};
+export const deleteTask = async (req, res) => {};
---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getTask,
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router.get("/:id", getTask);
+router.get("/", getTasks);
+router.get("/", createTask);
+router.get("/:id", updateTask);
+router.get("/:id", deleteTask);
